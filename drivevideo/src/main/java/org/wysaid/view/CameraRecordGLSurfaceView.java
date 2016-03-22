ab0b53454bbefd1280d29439b40d7146e82f0406
angcyo <angcyo@126.com>
2016-03-22 19:24:22 +0800

    add libstreaming

--- drivevideo/src/main/java/org/wysaid/view/CameraRecordGLSurfaceView.java
+++ drivevideo/src/main/java/org/wysaid/view/CameraRecordGLSurfaceView.java
@@ -6,14 +6,7 @@ package org.wysaid.view;
 
 
 import android.content.Context;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaRecorder;
 import android.util.AttributeSet;
-import android.util.Log;
-
-import java.nio.ByteBuffer;
-import java.nio.ShortBuffer;
 
 /**
  * Created by wangyang on 15/7/17.
@@ -23,6 +16,4 @@ public class CameraRecordGLSurfaceView extends CameraGLSurfaceView {
     public CameraRecordGLSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-
-    //Not provided by now.
 }
